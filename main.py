7455969af4e9a46a6d27b7f8eb7e078df19f1c2c
nishaalnaseer <nishawl.naseer@outlook.com>
2024-04-18 19:09:03 +0500

    setup

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import os
+
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
 
 from src.main import app
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    host = os.getenv('HOST')
+    port = int(os.getenv('PORT'))
+
+    uvicorn.run(
+        host=host,
+        port=port,
+        app=app
+    )
