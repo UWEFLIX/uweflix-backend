3a5c77dec1962e86e46153040af82b1bf26bb940
nishaalnaseer <nishawl.naseer@outlook.com>
2024-04-19 09:13:44 +0500

    db init script

--- src/utils/db_initialzation.py
+++ src/utils/db_initialzation.py
@@ -2,12 +2,18 @@ import asyncio
 from dotenv import load_dotenv
 from src.crud.db_management import close_db
 from src.crud.drop import create_new_db
-
+from src.crud.engine import db, host, port, user, password
 
 load_dotenv()
 
 
 async def main():
+    print(f"db: {db}")
+    print(f"host: {host}")
+    print(f"port: {port}")
+    print(f"user: {user}")
+    print(f"password: {password}")
+
     await create_new_db()
     await close_db()
 
