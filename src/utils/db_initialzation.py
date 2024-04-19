a2219335f1aac2b7cda4e34fa1290008ea420803
nishaalnaseer <nishawl.naseer@outlook.com>
2024-04-19 09:14:55 +0500

    db init script

--- src/utils/db_initialzation.py
+++ src/utils/db_initialzation.py
@@ -1,10 +1,11 @@
 import asyncio
 from dotenv import load_dotenv
+
+load_dotenv()
+
 from src.crud.db_management import close_db
 from src.crud.drop import create_new_db
 from src.crud.engine import db, host, port, user, password
-
-load_dotenv()
 
 
 async def main():
