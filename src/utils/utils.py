996a6878381e5083c2b7ea0d539467f490ef2e94
nishaalnaseer <nishawl.naseer@outlook.com>
2024-05-31 19:34:41 +0500

    saving openapi.json so it will be tracked in git

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,8 @@
+import asyncio
+import json
 import os
 import string
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from random import choice
 from fastapi import FastAPI
@@ -10,6 +13,30 @@ from aiofiles.os import makedirs, path
 
 
 ALPHABETS = list(string.ascii_uppercase)
+
+
+async def save_openai():
+    import urllib3
+    await asyncio.sleep(5)
+
+    http = urllib3.PoolManager()
+
+    port = os.getenv("PORT")
+
+    # Specify the URL
+    url = f"http://127.0.0.1:{port}/openapi.json"
+    loop = asyncio.get_event_loop()
+
+    with ThreadPoolExecutor() as executor:
+        response = await loop.run_in_executor(
+            executor,
+            http.request,
+            'GET', url
+        )
+
+    content = json.loads(response.data.decode('utf-8'))
+    with open("docs/openapi.json", "w") as f:
+        json.dump(content, f, indent=4)
 
 
 def generate_random_string(length=6) -> str:
@@ -37,6 +64,11 @@ def str_to_iso_format(_string: str):
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await create_assets_dir()
-    # await create_new_db()
+
+    is_dev = int(os.getenv('DEV'))
+    print(is_dev)
+    if is_dev:
+        asyncio.create_task(save_openai())
+
     yield
     await close_db()
